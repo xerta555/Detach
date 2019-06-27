6954d91050040ab2d042d04317bd1535ca073a06
Rom <skav@gmx.fr>
2019-06-27 19:06:11 +0200

    Update post-fs-data.sh

--- common/post-fs-data.sh
+++ common/post-fs-data.sh
@@ -1,7 +1,23 @@
 #!/system/bin/sh
-# Please don't hardcode /magisk/modname/... ; instead, please use $MODDIR/...
-# This will make your scripts compatible even if Magisk change its mount point in the future
+# Do NOT assume where your module will be located.
+# ALWAYS use $MODDIR if you need to know where this script
+# and module is placed.
+# This will make sure your module will still work
+# if Magisk change its mount point in the future
 MODDIR=${0%/*}
 
 # This script will be executed in post-fs-data mode
-# More info in the main Magisk thread
+
+alias sed=$(ls /sbin/.core/busybox/sed || ls /sbin/.magisk/busybox/sed) 2>/dev/null;
+instant_run=$MODDIR/instant_run.sh
+SERVICESH=$MODDIR/service.sh
+SYSAD=/system/addon.d
+iYTBSYSADD=$SYSAD/97-ytva.sh
+iYTBSYSADD_OLD=$SYSAD/97-ytva.sh.old
+iYTBSYSSYSADD=/system/$SYSAD/97-ytva.sh
+iYTBSYSSYSADD_OLD=/system/$SYSAD/97-ytva.sh.old
+
+if [ \( -e "$iYTBSYSADD" \) -o \( -e "iYTBSYSSYSADD" \) ]; then
+	mv -f "$iYTBSYSADD" "$iYTBSYSADD_OLD" 2>/dev/null
+	rm -f "$iYTBSYSSYSADD" "$iYTBSYSSYSADD_OLD" 2>/dev/null
+fi
